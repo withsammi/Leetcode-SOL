5874bf5eb5a7dea3f4e13dc405823d6c9fa3d04c
withsammi <106179911+withsammi@users.noreply.github.com>
2024-09-17 20:07:00 +0530

    Time: 24 ms (15.2%), Space: 25 MB (58.36%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -18,6 +18,7 @@ public:
             if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
                 return nums[mid];
             }
+            //
             if(mid%2==1 && nums[mid]==nums[mid-1] || mid%2==0 && nums[mid]==nums[mid+1]){
                 low = mid+1;
             }
