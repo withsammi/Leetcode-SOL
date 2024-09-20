183163f1f912082c1454fbbee9a74fd3fce80322
withsammi <106179911+withsammi@users.noreply.github.com>
2024-09-20 20:42:28 +0530

    Time: 96 ms (60%), Space: 116.2 MB (94.89%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.cpp
+++ 2155-find-missing-observations/2155-find-missing-observations.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+
+    void fun2(int n,int req,vector<int>&res){
+        int baseVal = req / n;
+        int rem = req % n;
+        res.assign(n, baseVal);
+        for (int i = 0; i < rem; i++) {
+            res[i]++;
+        }
+    }
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
         int m = rolls.size();
         int sum = accumulate(rolls.begin(), rolls.end(), 0);
@@ -7,12 +16,8 @@ public:
         if(req < n || req > 6*n){
             return {};
         }
-        int baseVal = req / n;
-        int rem = req % n;
-        vector<int> res(n, baseVal);
-        for (int i = 0; i < rem; i++) {
-            res[i]++;
-        }
+        vector<int> res;
+        fun2(n,req,res);
         return res;
     }
 };
